7b69e204c1c4a01b30e97fd55cfd30016af28d19
Lokesh Saipureddi <saipureddilokesh@gmail.com>
2025-02-01 07:41:23 -0500

    Update middleware.ts

--- ecommerce-frontend/middleware.ts
+++ ecommerce-frontend/middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    // Exclude Next.js internals and static files unless referenced in search params
+    '/((?!_next|.*\\.(html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Apply middleware to API and TRPC routes
     '/(api|trpc)(.*)',
   ],
 }
